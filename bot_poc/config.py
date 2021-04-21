9462a5e204584fbaccbb20097d65d89511a157a9
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-04-22 05:09:59 +0530

    Update config.py

--- bot_poc/config.py
+++ bot_poc/config.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 # 設置 App.config
-SECRET_KEY              = 'b07481bjava7704sd25sd27d53000e6b1daI52f47'
+SECRET_KEY              = '1BVtsOL8Buww2lx4-CKcdpf7s88BROYF-ef5DWKcFAmjvkQxi9oc1HEALR9vvTDplYW-0BBLZLMnKQ-IBK5ll2vLWqK4D8SXUHeOBzKaPzsTz0EZ5WsHz0QCqQiTGUx7Z94nvfGKspW2WX3uV4_8-pomNgPlRnNpmsgpb3IMJnPTXynL8FN2InfVRO-hxeifwC2hwRqVj9LtR7oZWA_W5q-Pd_dfh-WjyNdogbtt_mBvgbFMh5ykl7rWN6VOwc7P3hiQpbVobMWqtPWlFNmBao3Ld_8aRXmlewcRsOSh7duO9QQwhSljTOodl69wqPtCKMK7oOxP4jaL_rePbNivWGwqUjmmZqtk='
 SESSION_PROTECTION      = 'strong'
 JSON_AS_ASCII           = False
 #CSRF_ENABLED           = True
 TEMPLATES_AUTO_RELOAD   = True
 
 # 設置 bot_poc.config
-BOT_API_KEY             = "1234567890:ABCDEFGHIJKKKKKKKKKK"
-BOT_HOOK_URL            = "https://Domainnnnnnnnnnnnn/hook"
+BOT_API_KEY             = "1780957013:AAHxguOStH4BkEflW4NFnn1hcqhigUgD8H8"
+BOT_HOOK_URL            = "https://t.me/Autochatestbot.com/"
